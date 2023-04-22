9b85e8a5c1e64cc6e93304035c0d3c20fbcff5cc
shimojo <nao0909n@yahoo.co.jp>
2023-04-22 20:01:51 +0900

    vue.config.jsの内容変更

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,14 @@
 // vue.config.js
 module.exports = {
+  outputDir: 'docs',
+  assetsDir: './',
+  publicPath: './',
+  pages: {
+    index: {
+      entry: "src/main.js",
+      title: "cri_cafe"   
+    }
+  },
   css: {
     loaderOptions: {
       scss: {
